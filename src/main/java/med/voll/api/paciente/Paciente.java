f39f8464f79da45d95e18f4e81e4ff8e2fc2129b
Eduardo Serafim <eduserafim7@gmail.com>
2023-01-14 01:08:52 -0300

    Crição da tabela de pacientes  paciente no banco e validações

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -1,8 +1,21 @@
 package med.voll.api.paciente;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Paciente {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
@@ -10,5 +23,14 @@ public class Paciente {
     private String telefone;
     private String cpf;
 
+    @Embedded
     private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados){
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
