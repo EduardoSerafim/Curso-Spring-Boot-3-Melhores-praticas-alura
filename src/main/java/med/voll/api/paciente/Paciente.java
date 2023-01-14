cccd3c413696de3e2b50a0c0ec3201b62bbd5fe9
Eduardo Serafim <eduserafim7@gmail.com>
2023-01-14 00:12:48 -0300

    Cadastro do paciente

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,14 @@
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.Endereco;
+
+public class Paciente {
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+
+    private Endereco endereco;
+}
